f1aae71224c9a9eeb1dcca19518c668d1c11c9fa
PiotrDrechsler <pdrech@gmail.com>
2023-01-27 21:46:57 +0100

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
+import { useState, useEffect } from 'react';
+import { Notify } from 'notiflix';
+
+import s from './App.module.css';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { fetchHitsByQuery } from '../services/api';
+
 export const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setQuery(e.target.search.value);
+    setIsLoading(true);
+    setImages([]);
+    setPage(1);
+  };
+
+  const onNextPage = () => {
+    setPage(prevState => prevState + 1);
+    setIsLoading(true);
+  };
+
+  const onClickImage = url => {
+    setShowModal(true);
+    setLargeImageURL(url);
+  };
+
+  const onModalClose = () => {
+    setShowModal(false);
+    setLargeImageURL('');
+  };
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchGallery = async searchQuery => {
+      try {
+        const response = await fetchHitsByQuery(searchQuery, page);
+        setImages(prevState => [...prevState, ...response]);
+        if (response.length < 12) {
+          setShowBtn(false);
+        }
+        if (response.length === 12) {
+          setShowBtn(true);
+        }
+        if (response.length === 0) {
+          Notify.failure('No matches found!');
+        }
+      } catch (error) {
+        console.log('Error');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchGallery(query, page);
+  }, [page, query]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.App}>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery images={images} onClickImage={onClickImage} />
+      {isLoading && <Loader />}
+      {showBtn && <Button onNextPage={onNextPage} />}
+      {showModal && (
+        <Modal largeImageURL={largeImageURL} onModalClose={onModalClose} />
+      )}
     </div>
   );
 };
